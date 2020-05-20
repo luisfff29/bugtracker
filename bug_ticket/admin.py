89bdfaa9dab518248f2b891d12eb2356c584d291
luisfff29 <luisfff29@gmail.com>
2020-05-20 13:17:07 -0500

    create author(user) and ticket models

--- bug_ticket/admin.py
+++ bug_ticket/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from bug_ticket.models import Author, Ticket
+from django.contrib.auth.admin import UserAdmin
+
 
 # Register your models here.
+admin.site.register(Author, UserAdmin)
+admin.site.register(Ticket)
--- bug_ticket/models.py
+++ bug_ticket/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from bug_project import settings
+
 
 # Create your models here.
+
+STATUS_CHOICES = (('new', 'New'),
+                  ('in_progress', 'In Progress'),
+                  ('done', 'Done'),
+                  ('invalid', 'Invalid'))
+
+
+class Author(AbstractUser):
+    pass
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    time = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    status = models.CharField(max_length=128, choices=STATUS_CHOICES)
+    user_filed = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='filed')
+    user_assigned = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='assigned')
+    user_completed = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='completed')
