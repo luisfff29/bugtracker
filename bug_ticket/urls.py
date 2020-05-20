03603c457c218f1473c7e57d31fa041cc2b7f08b
luisfff29 <luisfff29@gmail.com>
2020-05-20 12:09:28 -0500

    create pages and urls

--- bug_ticket/urls.py
+++ bug_ticket/urls.py
@@ -3,4 +3,9 @@ from bug_ticket import views
 
 urlpatterns = [
     path('', views.main),
+    path('ticket/<int:id>/', views.details),
+    path('user/<str:name>/', views.user_profile),
+    path('edit/<int:id>/', views.edit),
+    path('login/', views.login_view),
+    path('signup/', views.signup_view),
 ]
--- bug_ticket/views.py
+++ bug_ticket/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
+
+
+def user_profile(request, name):
+    return render(request, 'profile.html')
+
+
+def details(request, id):
+    return render(request, 'details.html')
+
+
+def edit(request, id):
+    return render(request, 'edit.html')
+
+
+def login_view(request):
+    return render(request, 'login.html')
+
+
+def signup_view(request):
+    return render(request, 'signup.html')
