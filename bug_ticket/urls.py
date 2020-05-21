fe90dd46151f39421b846f21a371cef59f69c0fa
luisfff29 <luisfff29@gmail.com>
2020-05-21 18:51:31 -0500

    add invalid tickets button

--- bug_ticket/urls.py
+++ bug_ticket/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.main, name='home'),
     path('ticket/<int:id>/', views.details, name='details'),
     path('ticket/submit/', views.create_ticket, name='submit'),
-    path('ticket/invalid/', views.invalid_ticket),
+    path('ticket/invalid/', views.invalid_ticket, name='inv_tickets'),
     path('user/<str:name>/', views.user_profile, name='profile'),
     path('edit/<int:id>/', views.edit, name='edit'),
     path('action/<int:id>/', views.action, name='action'),
